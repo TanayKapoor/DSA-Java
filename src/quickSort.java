12fb9f19d1b32dd1ce1076a78e57ccd53cd064da
Tanay Kapoor <itstkaay@gmail.com>
2022-08-18 22:55:03 +0530

    Updates

---
+++ src/quickSort.java
@@ -0,0 +1,42 @@
+public class quickSort {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+    static int quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+        return 0;
+    }
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        System.out.println("Given array is");
+        printArray(arr);
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Sorted array is");
+        printArray(arr);
+    }
+}
